4a0feb76e91335674574e0aed5496d2a1ce69474
Luning He <heeluning@163.com>
2025-08-14 11:32:22 +0100

    upsert successed limited by batch size 96

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -6,6 +6,7 @@ import {pc} from "@/lib/pinecone";
 import { Md5 } from "ts-md5";
 
 export async function POST(req: Request) {
+    try {
     const formData = await req.formData();
     const file = formData.get('file') as File;
     console.log(file);
@@ -23,21 +24,23 @@ export async function POST(req: Request) {
     });
 
     const docs = await loader.load();
-    //  console.log(docs[0].pageContent);
 
     // 2. split docs into smaller chunks
-    //   text split
-    const splitDocs = await Promise.all(docs.map(doc =>  splitDoc(doc)));
-    // embedding pinecore
+    const splitDocs = await Promise.all(docs.map(doc => splitDoc(doc)));
+
+    // 3. upload to vector db
     const res = await Promise.all(splitDocs.map(embedChunks));
-    // console.log(res);
-
-
-    // upload to vector db
 
     return NextResponse.json({ message: "File uploaded successfully" })
+
+    } catch (error) {
+        console.error('Error uploading file:', error);
+        return NextResponse.json({ message: "File upload failed" });
+    }
 }
- const splitDoc = async (doc: Document) => {
+
+
+const splitDoc = async (doc: Document) => {
     const textSplitter = new RecursiveCharacterTextSplitter({
         chunkSize: 100,
         chunkOverlap: 0,
@@ -46,47 +49,17 @@ export async function POST(req: Request) {
       return texts
     }
 
+
 const embedChunks = async (chunks: string[]) => {
     const indexName = 'curio';
-    const model = 'multilingual-e5-large';
-
-        
-    // 创建索引（如果不存在）
-    try {
-        await pc.createIndexForModel({
-            name: indexName,
-            cloud: 'aws',
-            region: 'us-east-1',
-            embed: {
-                model: 'multilingual-e5-large',
-                fieldMap: { text: 'chunk_text' },
-            },
-            waitUntilReady: true,
-        });
-    } catch (error) {
-        console.log('Index might already exist:', error);
-    }
-
-    // const embeddings = await pc.inference.embed(
-    //     model,
-    //     chunks,
-    //     { inputType: 'passage', truncate: 'END' }
-    //   );
-
-    // creaet records with Md5
+    const nameSpace = 'namespace_1';
+    // create records with Md5
     const records = chunks.map((chunk, index) => ({
         _id: Md5.hashStr(chunk),
-        chunk_text: chunk,
+        text: chunk,
         category: 'pdf_document'
     }));
-    // const records = chunks.map((c, i) => ({
-    //     id: Md5.hashStr(c),
-    //     values: embeddings[i].values,
-    //     chunk_text: c,
-    //     metadata: {text:c}
-    //     category: 'pdf_document'
-    // }));
-    // console.log(records);
-    const index = pc.index(indexName).namespace("__default__");
+
+    const index = pc.index(indexName).namespace(nameSpace);
     return await index.upsertRecords(records)
 }
